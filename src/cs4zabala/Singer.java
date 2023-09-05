92a34769ec8d4b4e45cda87dd7146a9bc4817ec7
Emily Zabala <Emily Zabala@172.25.25.9>
2023-09-05 22:52:25 +0800

    Ex. 2 updates

---
+++ src/cs4zabala/Singer.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cs4zabala;
+
+/**
+ *
+ * @author Emily Zabala
+ */
+public class Singer {
+    String name;
+    int noOfPerformances;
+}
