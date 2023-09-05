f782885d71cc807926f694ffdba9401d15459b5a
Emily Zabala <Emily Zabala@172.25.25.9>
2023-09-05 22:15:27 +0800

    Ex. 2

---
+++ src/cs4zabala/CS4Ex2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cs4zabala;
+
+/**
+ *
+ * @author Emily Zabala
+ */
+public class CS4Ex2 {
+    public class Book {
+        String titleBo;
+        String author;
+        int pubYear;
+        double rating;
+    }
+    public class Song {
+        String titleSo;
+        String singer;
+        
+    }
+    public class Singer {
+        String name;
+        int noOfPerformances;
+            
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
+
